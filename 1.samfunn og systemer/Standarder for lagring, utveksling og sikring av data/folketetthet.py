b5c7bf913ff533655d37635430e8555be5c5e3cd
Jensbjorgo <snej765@gmail.com>
2023-05-30 12:13:54 +0200

    Gikk til slutt

--- 1.samfunn og systemer/Standarder for lagring, utveksling og sikring av data/folketetthet.py
+++ 1.samfunn og systemer/Standarder for lagring, utveksling og sikring av data/folketetthet.py
@@ -1,8 +1,6 @@
 #3. Bruk filen `List of world cities by population density.csv` og finn ut hvilket land som har flest byer på topp-50 listen av byer med høyest befolkningstetthet.
 
-import matplotlib.pyplot as plt
-
-fil = open("samfunn og systemer/Standarder for lagring, utveksling og sikring av data/liste.csv")
+fil = open("1.samfunn og systemer/Standarder for lagring, utveksling og sikring av data/liste.csv")
 data = fil.read()
 linjer = data.split("\n")
 
@@ -17,4 +15,39 @@ print(land)
 # Finn landet med flest byer i topp-50 listen
 land_med_flest_byer = max(land, key=land.get)
 
+print("--------------------------------")
+
 print(f"Landet med flest byer i topp 50 er {land_med_flest_byer} med {land['India']}")
+print("-------------------------------")
+#4. Bruk filen AirBnb-Europe.csv og finn ut hvilken by som er billigst å leie leilighet i
+
+fil2=open("1.samfunn og systemer/Standarder for lagring, utveksling og sikring av data/AirBnb-Europe.csv")
+data2=fil2.read()
+linjer2=data2.split("\n")
+
+priser={}
+for linje in linjer2[1:-1]:
+    kolonne=linje.split(",")
+    by=kolonne[0]
+    pris=float(kolonne[1])
+    if by in priser:
+        priser[by].append(pris)
+    else:
+        priser[by]=[pris]
+
+snitt={}
+for by, verdier in priser.items():
+    snittverdi=sum(verdier)/len(verdier)
+    snitt[by]= snittverdi
+
+minst=1000000
+billigste_by= " "
+
+for by,snittverdi in snitt.items():
+    if snittverdi<minst:
+        minst=snittverdi
+        billigste_by=by
+    print(f"Den gjennomsnittlige prisen i {by} er {snittverdi}")
+
+print("------------------")
+print(f"Den billigste byen er derfor {billigste_by} med en gjennomsnittsptis på {minst}")
--- 2.problemløsning/alternativelosninger.md
+++ 2.problemløsning/alternativelosninger.md
@@ -4,3 +4,28 @@ For å ikke sitte fast når du skal programmere er det viktig å kunne bruke alt
 ## Mulig løsning for et roulette spill
 - [Roulettespill](https://github.com/Jensbjorgo/IT2-bigmppe/tree/main/diverse/Roulette.py)
 
+
+## Forskjellige måter å åpne en csv-fil
+python````
+
+fil = open("tentamenmai/pokemon.csv") # åpner filen pokemon.csv
+data = fil.read() # leser innholdet i filen
+linjer = data.split("\n") # splitter innholdet på linjeskift, slik at linjer blir en liste med linjer
+
+
+import csv
+
+filnavn=("tentamenmai/pokemon.csv")
+
+with open(filnavn,encoding="iso8859-1") as fil: # Åpner filen
+    data = csv.reader(fil, delimiter=",")         # Leser inn data som csv
+
+
+
+with open(filnavn,encoding="iso8859-1") as fil: # Åpner filen
+    data = csv.DictReader(fil, delimiter=",")   # Leser inn data som csv men i ordbok
+
+
+
+
+```
